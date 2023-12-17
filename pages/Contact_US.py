25c22838777a1f0df0544e2deb420c6f77e9c5af
Anish Gopalakrishnan <anish.aws@gmail.com>
2023-12-16 19:22:16 -0500

    Updated Contact US and Email functionality

--- pages/Contact_US.py
+++ pages/Contact_US.py
@@ -7,9 +7,9 @@ with st.form(key="email_forms"):
     user_email = st.text_input("Your Email Address")
     raw_message = st.text_area("Your message")
     button = st.form_submit_button("Submit")
-    message =f"""\
+    message = f"""\
     Subject: New email from {user_email}
-    
+
     From: {user_email}
     {raw_message}
     """
--- send_email.py
+++ send_email.py
@@ -1,12 +1,13 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(user_email, message):
     host = "smtp.gmail.com"
     port = 465
-    username = "rob.kelly.python@gmail.com"
-    password = "vsvwdjkprlqjywgj"
+    username = ""
+    password = os.getenv("PASSWORD")
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
